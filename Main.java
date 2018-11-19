ed9ea75d70938e4096e348a8dcbbd45dec5f83d5
dalia-maher <dalia.maher94@gmail.com>
2018-11-19 13:56:05 +0200

    Adding print function

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class DemoJavaProject {
 	public static void main(String [] args) {
+		print();
+	}
+	
+	public static void print() {
 		System.out.println("Hello Java");
 		System.out.println("Hello XYZ");
 	}
